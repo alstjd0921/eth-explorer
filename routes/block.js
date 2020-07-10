fb2598fb261563d95deb9af4681f586dd5a670ca
alstjd0921 <kwonarseus@gmail.com>
2020-07-10 14:29:19 +0900

    Add block router. Apply eslint.

---
+++ routes/block.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const router = express.Router();
+const Web3 = require('web3');
+
+const addComma = (num) => {
+  let regexp = /\B(?=(\d{3})+(?!\d))/g;
+  return num.toString().replace(regexp, ',');
+};
+
+router.use(express.urlencoded({ extended: false }));
+
+router.get('/:pageId', async function (req, res) {
+  web3 = new Web3(
+    new Web3.providers.HttpProvider(
+      'https://mainnet.infura.io/v3/16275c322c5a47ad8fd125cc110ff860'
+    )
+  );
+  let pageId = req.params.pageId;
+
+  await web3.eth.getBlockNumber(function (err, rtn) {
+    if (pageId > rtn) {
+      return res.render('error');
+    }
+  });
+
+  if (pageId < 1700000) {
+    txFee = 5000000000000000000;
+  } else if (pageId < 4230000) {
+    txFee = 3000000000000000000;
+  } else {
+    txFee = 2000000000000000000;
+  }
+
+  await web3.eth.getBlock(pageId, false, async function (err, block) {
+    var timestamp = block.timestamp * 1000;
+    var date = new Date(timestamp);
+    for (let i = 0; i <= block.transactions.length - 1; i++) {
+      await web3.eth.getTransaction(
+        block.transactions[i],
+        false,
+        async function (err, tx) {
+          await web3.eth.getTransactionReceipt(
+            block.transactions[i].toString(),
+            false,
+            async function (err, txReceipt) {
+              txFee += tx.gasPrice * txReceipt.gasUsed;
+            }
+          );
+        }
+      );
+    }
+    setTimeout(function () {
+      return res.render('block', {
+        pageId: pageId,
+        blockNumber: block.number,
+        blockTimestamp: date,
+        blockTransactionsLength: block.transactions.length,
+        blockMiner: block.miner,
+        blockDifficulty: addComma(block.difficulty),
+        blockTotalDifficulty: addComma(block.totalDifficulty),
+        blockSize: addComma(block.size),
+        blockGasUsed: addComma(block.gasUsed),
+        blockGasLimit: addComma(block.gasLimit),
+        blockExtraData: block.extraData,
+        blockHash: block.hash,
+        blockParentHash: block.parentHash,
+        blockSha3Uncles: block.sha3Uncles,
+        blockNonce: block.nonce,
+        blockReward: web3.utils.fromWei(txFee.toString(10)),
+      });
+    }, 500);
+  });
+});
+
+module.exports = router;
